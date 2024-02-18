b17cde0b3030641814f3a68de4fa0bfde17ea729
Cevin <niconiocevin@gmail.com>
2024-02-18 12:38:54 +0700

    Tambah pegawai

---
+++ src/main/java/com/example/db_android/MainActivity.java
@@ -0,0 +1,86 @@
+package com.example.db_android;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.annotation.SuppressLint;
+import android.app.ProgressDialog;
+import android.os.AsyncTask;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import com.example.db_android.R;
+import com.example.db_android.RequestHandler;
+import com.example.db_android.konfigurasi;
+
+import java.util.HashMap;
+
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+
+    private EditText editTextName;
+    private EditText editTextDesg;
+    private EditText editTextSal;
+
+    private Button buttonAdd;
+
+    @SuppressLint("MissingInflatedId")
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        editTextName = findViewById(R.id.editTextName);
+        editTextDesg = findViewById(R.id.editTextDesg);
+        editTextSal = findViewById(R.id.editTextSalary);
+
+        buttonAdd = findViewById(R.id.buttonAd);
+        buttonAdd.setOnClickListener(this);
+    }
+
+    private void addEmployee() {
+        final String name = editTextName.getText().toString().trim();
+        final String desg = editTextDesg.getText().toString().trim();
+        final String sal = editTextSal.getText().toString().trim();
+
+        class AddEmployee extends AsyncTask<Void, Void, String> {
+
+            ProgressDialog loading;
+
+            @Override
+            protected void onPreExecute() {
+                super.onPreExecute();
+                loading = ProgressDialog.show(MainActivity.this, "Menambahkan...", "Tunggu...", false, false);
+            }
+
+            @Override
+            protected void onPostExecute(String s) {
+                super.onPostExecute(s);
+                loading.dismiss();
+                Toast.makeText(MainActivity.this, s, Toast.LENGTH_LONG).show();
+            }
+
+            @Override
+            protected String doInBackground(Void... v) {
+                HashMap<String, String> params = new HashMap<>();
+                params.put(konfigurasi.KEY_EMP_NAMA, name);
+                params.put(konfigurasi.KEY_EMP_POSISI, desg);
+                params.put(konfigurasi.KEY_EMP_GAJI, sal);
+                RequestHandler rh = new RequestHandler();
+                String res = rh.sendPostRequest(konfigurasi.URL_ADD, params);
+                return res;
+            }
+        }
+
+        AddEmployee ae = new AddEmployee();
+        ae.execute();
+    }
+
+    @Override
+    public void onClick(View v) {
+        if (v == buttonAdd) {
+            addEmployee();
+        }
+    }
+}
